e0e36291ba00cf47bed1f28dad41920afd95cb4f
Anuebunwa Adanna <anuebunwaadanna3@gmail.com>
2024-12-21 23:22:38 +0100

    goals_assign.sh

---
+++ goals_assign.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define functions
+who_am_i() {
+  echo "Hello, I am Anuebunwa Adanna!"
+}
+
+fullname() {
+  echo "Fullname: Anuebunwa Adanna"
+}
+
+Bio() {
+  echo "Bio: Adanna is an intending cloud engineer who was raised in Enugu, Nigeria."
+  echo "My hobbies are singing, dancing, and sleeping."
+  echo "My life currently revolves around work, church, and home, but I have a few good friends in my corner."
+}
+
+year_in_tech() {
+  echo "Year in Tech: I have been in tech for 3 years, mostly as a product manager."
+}
+
+dream_job() {
+  echo "Dream Job: Cloud Engineer for AWS."
+}
+
+goals() {
+  echo "To gain hands-on experience and apply knowledge to solve real-world problems."
+}
+
+# Call functions
+echo "About Me:"
+who_am_i
+fullname
+Bio
+year_in_tech
+dream_job
+goal
